0fc0e92b5d59381e4b96e3b264cfb7c46aa04fcc
Wesley Breukers <wesley@vertcodedevelopment.com>
2023-07-19 22:20:31 +0200

    fix: vote4 redirect

--- src/pages/vote4.tsx
+++ src/pages/vote4.tsx
@@ -1,5 +1,13 @@
-export default function Vote1Page() {
-    return (<></>);
-}
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 // Redirect to https://minecraft.buzz/vote/8163
+export default function Vote4Page() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('https://minecraft.buzz/vote/8163').catch(console.log);
+    });
+
+    return (<></>);
+}
